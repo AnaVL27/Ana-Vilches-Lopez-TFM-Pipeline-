db3ca4a42e13fab663b42e93feda4bbac76770d1
AnaVL27 <anavilcheslopez@gmail.com>
2026-06-24 13:25:17 +0200

    Add files via upload

---
+++ 0-manifest_create.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Manifest file to be created automatically to perform any QIIME analysis
+#It runs from the same folder where the fastq sequences are.
+
+###################################################################################################
+#First edit this variable name:
+###################################################################################################
+
+MANIFEST=TFM-manifest
+
+
+#Check that this part of the script: 
+
+ls *R1* |
+  while read sample
+    do
+      prefix="${sample%_R1*}"
+      id=$(echo "$sample" | cut -d'_' -f1,2)
+      echo "$id,$PWD/${prefix}_R1_001.fastq.gz,forward"
+      echo "$id,$PWD/${prefix}_R2_001.fastq.gz,reverse"
+      
+    done >> $MANIFEST ;
+
+echo 'Finished' ;
+echo 'Remember to check the manifest file has been properly created'
